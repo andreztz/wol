25a9fe41afe2ee9ea7b40338e0501de267eb60af
andreztz <andreztz@gmail.com>
2018-08-11 02:39:46 -0300

    add argparser & helps for cli

--- wol.py
+++ wol.py
@@ -1,13 +1,18 @@
-# wol.py
+#!/usr/bin/env python
+'''
+Switches on remote computers using WOL.
+
+'''
 # https://www.vivaolinux.com.br/artigo/Wake-on-LAN-WOL-utilizando-Netcat-Dissecando-o-protocolo
 
 import socket
 import struct
-import sys
-
+import re
 
 def wake_on_lan(macaddress):
-    """ Switches on remote computers using WOL. """
+    ''' Use macaddresses with any seperators.
+    '0F:0F:DF:0F:BF:EF', '0F-0F-DF-0F-BF-EF',
+    or without any seperators '0F0FDF0FBFEF' '''
 
     # Check macaddress format and try to compensate.
     if len(macaddress) == 12:
@@ -16,7 +21,7 @@ def wake_on_lan(macaddress):
         sep = macaddress[2]
         macaddress = macaddress.replace(sep, '')
     else:
-        raise ValueError('Incorrect MAC address format')
+        raise ValueError('Incorrect MAC address format.')
 
     # Pad the synchronization stream.
     data = ''.join(['FFFFFFFFFFFF', macaddress * 20])
@@ -26,8 +31,7 @@ def wake_on_lan(macaddress):
     # Split up the hex values and pack.
     for i in range(0, len(data), 2):
         send_data = b''.join(
-            [send_data, struct.pack('B', int(data[i: i + 2], 16))]
-        )
+                [send_data, struct.pack('B', int(data[i: i + 2], 16))])
 
     # Broadcast it to the LAN.
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -36,9 +40,14 @@ def wake_on_lan(macaddress):
 
 
 if __name__ == '__main__':
-    # Use macaddresses with any seperators.
-    # wake_on_lan('0F:0F:DF:0F:BF:EF')
-    # wake_on_lan('0F-0F-DF-0F-BF-EF')
-    # or without any seperators.
-    # wake_on_lan('0F0FDF0FBFEF')
-    wake_on_lan(sys.argv[1])
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, 
+        usage='wol --mac "0F:0F:DF:0F:BF:EF"')
+    parser.add_argument('-m', '--mac', help=wake_on_lan.__doc__)
+    args = parser.parse_args()
+    try:
+        wake_on_lan(args.mac)
+    except Exception as error:
+        print(error)
